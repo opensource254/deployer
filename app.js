983efab4f876c5c4cea356825b577a8d4ca38af7
Stanley Masinde <stanleyloren@gmail.com>
2020-06-16 12:10:57 +0300

    (Refactor). * Using /github route for Github webhook. This would be helpful when we want to add other webhook sources like Bitbucket. * An empty handler for bitbucket has also been added.

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+const githubRouter = require('./routes/github');
+const bitbucketRouter = require('./routes/bitbucket')
 
 const app = express();
 
@@ -12,7 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
+app.use('/github', githubRouter);
+app.use('/bitbucket', bitbucketRouter);
 app.all('*', (req, res) => {
     res.status(404).json({ error: `Sorry you cannot ${req.method} ${req.path}.` })
 })

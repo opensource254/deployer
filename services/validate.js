7ab2a845fad350b6fef08780a6105bbde4f16627
Stanley Masinde <stanmasinde@gmail.com>
2020-06-18 10:54:19 +0300

    Addition of more handlers. * The bitbucket handler now accepts post requests via POST /bitbucket * The validator for the above hook has moved to ./services/validator * The deployment for the above hook in in the ./services/deployment This commit contains a significant refactor to enable scalability of the app. N/B the github handler has not been refactored.     No support for GitLab yet

---
+++ services/validate.js
@@ -0,0 +1,47 @@
+const fs = require('fs')
+
+/**
+ * ------------------------------------
+ * Validate the incoming webhook
+ * ------------------------------------
+ */
+class Validate {
+    /**
+     * Load the configuration file
+     */
+    static deploymentConfig = JSON.parse(fs.readFileSync('deployment.config.json', 'utf-8'))
+    /**
+     * Validate the bitbucket payload
+     * @param {Array} body 
+     * @returns Promise
+     */
+    static bitbucket = (body = []) => new Promise((resolve, reject) => {
+
+        /** Ensure the request body has a repository field */
+        if (!body.repository) {
+            reject({ status: 400, message: "Repository Parameter not found in the body" })
+            return
+        }
+
+        /** It is safe to decalre this variable because the param exists */
+        const repositoryName = body.repository.project.name
+
+        /** Get the config from the config file */
+        const [repositoryConfig] = this.deploymentConfig.filter((config) => {
+            return config.name === repositoryName
+        })
+
+        /** Tell the user about a missing config */
+        if (!repositoryConfig) {
+            reject({ message: `Config entry for ${repositoryName} was not found in the config file` })
+            return
+        }
+
+        /** Everthing looks good resolve the reposory name */
+        resolve(repositoryConfig)
+    })
+
+    // TODO Add Github and Gitlab here
+}
+
+module.exports = Validate

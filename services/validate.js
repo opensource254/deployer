355e188699188ec6a124c8a08e61eb601df34f34
Stanley Masinde <stanmasinde@gmail.com>
2020-06-19 10:21:39 +0300

    Refactored the validate body method. * This will make it easy to add more rules to this method

--- services/validate.js
+++ services/validate.js
@@ -10,6 +10,17 @@ class Validate {
      * Load the configuration file
      */
     static deploymentConfig = JSON.parse(fs.readFileSync('deployment.config.json', 'utf-8'))
+
+    /**
+     * Validate the payload's body
+     * @param {Array} payload 
+     */
+    static #validateBody = (payload = []) => {
+        if (payload.repository) {
+            return true
+        }
+        return false
+    }
     /**
      * Validate the bitbucket payload
      * @param {Array} body 
@@ -18,9 +29,8 @@ class Validate {
     static bitbucket = (body = []) => new Promise((resolve, reject) => {
 
         /** Ensure the request body has a repository field */
-        if (!body.repository) {
-            reject({ status: 400, message: "Repository Parameter not found in the body" })
-            return
+        if (!this.#validateBody(body)) {
+            return reject({ message: "Invalid Payload" })
         }
 
         /** It is safe to decalre this variable because the param exists */
@@ -46,15 +56,14 @@ class Validate {
      * @param {Array} body 
      */
     static github = (body = []) => new Promise((resolve, reject) => {
-         /** Ensure the request body has a repository field */
-         if (!body.repository) {
-            reject({ status: 400, message: "Repository Parameter not found in the body" })
-            return
+        /** Ensure the request body has a repository field */
+        if (!this.#validateBody(body)) {
+            return reject({ message: "Invalid Payload" })
         }
 
         /** It is safe to decalre this variable because the param exists */
         const repositoryName = body.repository.name
-        
+
 
         /** Get the config from the config file */
         const [repositoryConfig] = this.deploymentConfig.filter((config) => {
@@ -71,35 +80,34 @@ class Validate {
         resolve(repositoryConfig)
     })
 
-     /**
-     * Validate the Gitlab's payload
-     * @param {Array} body 
-     */
+    /**
+    * Validate the Gitlab's payload
+    * @param {Array} body 
+    */
     static gitlab = (body = []) => new Promise((resolve, reject) => {
         /** Ensure the request body has a repository field */
-        if (!body.repository) {
-           reject({ status: 400, message: "Repository Parameter not found in the body" })
-           return
-       }
+        if (!this.#validateBody(body)) {
+            return reject({ message: "Invalid Payload" })
+        }
 
-       /** It is safe to decalre this variable because the param exists */
-       const repositoryName = body.repository.name
-       
+        /** It is safe to decalre this variable because the param exists */
+        const repositoryName = body.repository.name
 
-       /** Get the config from the config file */
-       const [repositoryConfig] = this.deploymentConfig.filter((config) => {
-           return config.name === repositoryName
-       })
 
-       /** Tell the user about a missing config */
-       if (!repositoryConfig) {
-           reject({ message: `Config entry for ${repositoryName} was not found in the config file` })
-           return
-       }
+        /** Get the config from the config file */
+        const [repositoryConfig] = this.deploymentConfig.filter((config) => {
+            return config.name === repositoryName
+        })
 
-       /** Everthing looks good resolve the reposory name */
-       resolve(repositoryConfig)
-   })
+        /** Tell the user about a missing config */
+        if (!repositoryConfig) {
+            reject({ message: `Config entry for ${repositoryName} was not found in the config file` })
+            return
+        }
+
+        /** Everthing looks good resolve the reposory name */
+        resolve(repositoryConfig)
+    })
 }
 
 module.exports = Validate

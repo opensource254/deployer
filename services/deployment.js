56e02acd7beb6fd2b6f945da7458a39514373493
Stanley Masinde <stanmasinde@gmail.com>
2020-06-26 13:57:20 +0300

    [Notifications] The following changes were made in this commit. * Created the notifications config file under config folder * Created the notification service * Sending uncontrolled Slack notifications

--- services/deployment.js
+++ services/deployment.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const { exec } = require('child_process')
+const NotificationService = require('./notifications')
 
 /**
  * Handle deployment
@@ -9,8 +10,10 @@ const deployment = (Config = []) => {
     exec(`${Config.command}`, (_err, stdout, stderr) => {
         if (stdout) {
             // TODO Make this available to the user
+            new NotificationService(stdout, 'success').sendNotifications()
         }
         if (stderr) {
+            new NotificationService(stderr, 'error').sendNotifications()
             fs.writeFileSync('error.log', `${new Date().toUTCString()}, Config: ${Config.name}, Error: ${stderr}`, {
                 encoding: 'utf-8',
                 flag: 'a',

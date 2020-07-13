fdd2ae3b045635d222d52c1bf05c92b17308b42f
Stanley Masinde <stanmasinde@gmail.com>
2020-07-13 12:19:11 +0300

    Start of the web interface

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 require('dotenv').config()
 const express = require('express')
-const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
 const githubRouter = require('./routes/github')
 const bitbucketRouter = require('./routes/bitbucket')
 const gitlabRouter = require('./routes/gitlab')
-// const verifyHostName = require('./middleware/verifyHostName')
+const web = require('./web/web')
 
 const app = express();
 
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
 // TODO This middleware does not work correctly app.use(verifyHostName())
 
 app.use('/github', githubRouter)
 app.use('/bitbucket', bitbucketRouter)
 app.use('/gitlab', gitlabRouter)
-app.all('*', (req, res) => {
-    res.status(404).json({ error: `Sorry you cannot ${req.method} ${req.path}.` })
-})
+app.all('*', web)
 
 module.exports = app;

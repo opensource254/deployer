7360077421ce439a1736b8983aa1abed24da5103
Stanley Masinde <stanmasinde@gmail.com>
2020-07-17 22:58:51 +0300

    Added knex config for migrations and schema. * Created the users table migrations * Configured the database connection for Knex CLI

---
+++ knexfile.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+
+  staging: {
+    client: 'mysql',
+    connection: {
+      database: 'deployer',
+      user: '',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'migrations'
+    }
+  },
+
+  production: {
+    client: process.env.DATABASE_CLIENT,
+    connection: {
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'migrations'
+    }
+  }
+
+};
---
+++ migrations/20200717220234_create_users_table.js
@@ -0,0 +1,15 @@
+const { table } = require('../web/app/models/database');
+
+/**@var knex = require('knex')() */
+exports.up = function (knex) {
+    return knex.schema.createTable('users', (table) => {
+        table.bigIncrements('id');
+        table.string('name').nullable();
+        table.string('email').unique();
+        table.string('password');
+    })
+};
+
+exports.down = function (knex) {
+    knex.schema.dropTable('users');
+};

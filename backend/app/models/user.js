11e3cc281e2968df4878823a636bc396842edda5
Stanley Masinde <stanmasinde@gmail.com>
2021-05-02 21:29:27 +0300

    Command to create a user

--- backend/app/models/user.js
+++ backend/app/models/user.js
@@ -1,4 +1,16 @@
 const { Model } = require('mevn-orm')
+const { hashSync } = require('bcrypt')
 
-class User extends Model {}
+class User extends Model {
+  /**
+   * Register a new user
+   * @param {*} details
+   */
+  static register(details = []) {
+    const { password } = details
+    const hash = hashSync(password, 10)
+    details.password = hash
+    return this.create(details)
+  }
+}
 module.exports = User

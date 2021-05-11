94677d7fc7e220936149a8a54c59ad975c29f6e0
Stanley Masinde <stanmasinde@gmail.com>
2021-05-11 15:26:34 +0300

    Updated the seeds

--- backend/database/seeds/create_users.js
+++ backend/database/seeds/create_users.js
@@ -1,13 +1,13 @@
-
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('table_name')
+    .del()
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+        { id: 1, colName: 'rowValue1' },
+        { id: 2, colName: 'rowValue2' },
+        { id: 3, colName: 'rowValue3' },
+      ])
+    })
+}

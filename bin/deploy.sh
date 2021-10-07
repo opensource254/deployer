97932a5972294427916935e32fb3ca19c8dbe692
Stanley Masinde <stanmasinde@gmail.com>
2021-10-07 18:11:33 +0300

    Updated the deployment script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -28,50 +28,42 @@ if [ -z "$git_branch" ]; then
    git_branch="main"
 fi
 
-echo "$(date) - Starting deployment"
-echo "$(date) - Current user: $(whoami)"
-echo "$(date) - Current directory: $(pwd)"
+echo "$(date) - Starting deployment the current user is $(whoami)"
+echo "$(date) - System load is $(uptime)"
 
 
 # get the folder name from the repository url
 folder_name=$(basename $repository_url | cut -d. -f1)
 
-echo "Deploying $folder_name to $deployment_dir"
+echo "$(date) - Deploying $folder_name to $deployment_dir"
 
 HOME_DIR=$(eval echo ~$(whoami))
-echo "Using  $HOME_DIR as home directory"
+echo "$(date) - Using  $HOME_DIR as home directory"
 DEPLOY_DIR=$HOME_DIR/deploy
 echo "Using  $DEPLOY_DIR as deploy directory"
 ## create the deploy directory if it does not exist
-echo "Checking if $DEPLOY_DIR exists"
+echo "$(date) -Checking if $DEPLOY_DIR exists"
 if [ ! -d $DEPLOY_DIR ]; then
     mkdir $DEPLOY_DIR
 fi
-echo "Switching to $DEPLOY_DIR"
+echo "$(date) - Switching to $DEPLOY_DIR"
 cd $DEPLOY_DIR
 
 ## Delete deployent folder if it exists
 if [ -d "$folder_name" ]; then
-    echo "Deleting old files"
+    echo "$(date) - Deleting old files"
     rm -rf $folder_name
 fi
 
 git clone  $repository_url --depth=1 -b $git_branch
-echo "$(date) - Current branch: $(git rev-parse --abbrev-ref HEAD)"
-echo "$(date) - Current commit: $(git rev-parse HEAD)"
-echo "$(date) - Current commit message: $(git log -1 --pretty=%B)"
-echo "$(date) - Current commit author: $(git log -1 --pretty=%an)"
-echo "$(date) - Current commit date: $(git log -1 --pretty=%ad)"
-echo "Removing .git folder"
+echo "$(date) - Removing .git folder"
 rm -rf $DEPLOY_DIR/$folder_name/.git
-echo "Copying files from $DEPLOY_DIR/$folder_name to $deployment_dir"
+echo "$(date) - Copying files from $DEPLOY_DIR/$folder_name to $deployment_dir"
 cp -rf $DEPLOY_DIR/$folder_name $deployment_dir
-echo "Deleting the $DEPLOY_DIR/$folder_name directory"
+echo "$(date) - Deleting the $DEPLOY_DIR/$folder_name directory"
 rm -rf $DEPLOY_DIR/$folder_name
-echo "switching to $deployment_dir$folder_name"
+echo "$(date) - switching to $deployment_dir$folder_name"
 cd $deployment_dir$folder_name
-echo "Running post deployment script"
-## check for the existtense of any of theses files update.sh, deploy.sh, postdeploy.sh
-## if they exist then run the script
 
-echo "Please run your update script manually."
+echo "$(date) - Deployment finished successfully"
+exit 0
